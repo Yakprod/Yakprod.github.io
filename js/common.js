62048cb1cd45662ac1d9103da6708afd45e02aa9
Yakprod <nosanosaasi@gmail.com>
2017-05-27 03:25:13 +0300

    minor changes

--- js/common.js
+++ js/common.js
@@ -52,11 +52,11 @@ $(document).ready(function() {
 		};
 	});
 
-	$(".portfolio_item").each(function(i) {
-		$(this).find("a").attr("href", "#work_" + i);
-		$(this).find(".podrt_descr").attr("id", "work_" + i);
-	});
-
+	// $(".portfolio_item").each(function(i) {
+	// 	$(this).find("a").attr("href", "#work_" + i);
+	// 	$(this).find(".podrt_descr").attr("id", "work_" + i);
+	// });
+	
 	$("input, select, textarea").jqBootstrapValidation();
 
 	$(".top_mnu ul a").mPageScroll2id();
